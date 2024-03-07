666a6ca79487066a5ccac0bff3f23ed806f44f69
mistichris <codeine500@hotmail.com>
2024-03-06 22:25:17 -0600

    initial commit-repo setup

---
+++ src/test/java/dmacc/SpringContacts2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContacts2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
