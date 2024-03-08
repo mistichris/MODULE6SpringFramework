66f07c0e8f931ba7449b90288bef6136ff6196cf
mistichris <codeine500@hotmail.com>
2024-03-07 22:53:04 -0600

    "final updates"

--- src/test/java/dmacc/SpringContacts2024ApplicationTests.java
+++ src/test/java/dmacc/SpringContacts2024ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringContacts2022ApplicationTests {
+class SpringContacts2024ApplicationTests {
 
 	@Test
 	void contextLoads() {
